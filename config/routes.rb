afc3f30c5a4a722f50b10fdd869e6e9010f64953
zaka0406 <zaka0406@gmail.com>
2023-06-20 19:17:37 +0900

    second save

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'index', to: 'app_saika#index'
+  root to: "saikas#index"
+
+  resources :saikas,only:[:index] do
+  # root追加
+    collection do
+      get "about"
+    end
+  end
 
 end
--- db/migrate/20230611092512_create_saikas.rb
+++ db/migrate/20230611092512_create_saikas.rb
@@ -2,6 +2,8 @@ class CreateSaikas < ActiveRecord::Migration[6.1]
   def change
     create_table :saikas do |t|
       t.string :name
+      t.string :text
+      t.text :image
   
       t.timestamps
     end
