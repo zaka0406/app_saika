8e485578b2be79230792faef59c8f95863df9a71
zaka0406 <zaka0406@gmail.com>
2023-08-25 18:17:52 +0900

    render用に追加設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,17 +118,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { protocol: 'https', host:'https://saikaapp0312.onrender.com/saikas'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
     port:                 587,
-    user_name:            'saikachihiro.3588@gmail.com',
-    # ENV['SMTP_USERNAME'],
-    password:             'kexjcofunyipgmmp',
-    # ENV['SMTP_PASSWORD'],
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
     authentication:       'login',
     enable_starttls_auto: true
   }
