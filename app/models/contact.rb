0596e0955132e0ec79e389c0bbd3769dfc03dfdb
zaka0406 <122841734+zaka0406@users.noreply.github.com>
2023-09-01 19:20:56 +0900

    render用に追加設定変更

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
-
-   
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :phone_number, presence: true
+    validates :message, presence: true
 end
