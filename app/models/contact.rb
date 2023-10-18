6c26f2ed83b2350ddc4a383bf12f36dc117b4387
zaka0406 <122841734+zaka0406@users.noreply.github.com>
2023-10-18 19:37:54 +0900

    render用に追加設定変更

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :phone_number, presence: true, format: { with: /\A\d{10}\z/ }
+    validates :phone_number, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
     validates :message, presence: true
 end
