7340132811aa1e9d7b1d46db3353cc9c51a8590e
zaka0406 <122841734+zaka0406@users.noreply.github.com>
2023-10-18 15:54:34 +0900

    render用に追加設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,7 @@ Rails.application.configure do
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   # config/environments/production.rb
-  config.active_record.default_url_options = { adapter: 'postgresql', database: 'saika_app_db_28fl', username: 'saika_app_db_28fl_user', password:ENV['production.DB_PASSWORD']
-  , host: 'localhost' }
+  config.active_record.default_url_options = { adapter: 'postgresql', database: 'saika_app_db_28fl', username: 'saika_app_db_28fl_user', password:ENV['production.DB_PASSWORD'], host: 'localhost' }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
